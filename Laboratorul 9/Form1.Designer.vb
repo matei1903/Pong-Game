0997e92e8d7da5f7e3d91ea3c2b63f3bb32afa95
Matei-Alexandru Ciucă <86956363+matei1903@users.noreply.github.com>
2023-02-23 01:10:05 +0200

    Add files via upload

---
+++ Laboratorul 9/Form1.Designer.vb
@@ -0,0 +1,159 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.miscare_bila = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.control_obstacol = New System.Windows.Forms.PictureBox()
+        Me.control_bila = New System.Windows.Forms.PictureBox()
+        Me.control_paleta = New System.Windows.Forms.PictureBox()
+        Me.obstacol2 = New System.Windows.Forms.PictureBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        CType(Me.control_obstacol, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.control_bila, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.control_paleta, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.obstacol2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'miscare_bila
+        '
+        Me.miscare_bila.Enabled = True
+        Me.miscare_bila.Interval = 10
+        '
+        'Label1
+        '
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(715, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(40, 20)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Vieti : "
+        '
+        'Label2
+        '
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(752, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 20)
+        Me.Label2.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(700, 29)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(55, 16)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Puncte :"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(752, 29)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(0, 16)
+        Me.Label4.TabIndex = 5
+        '
+        'control_obstacol
+        '
+        Me.control_obstacol.Location = New System.Drawing.Point(508, 83)
+        Me.control_obstacol.Name = "control_obstacol"
+        Me.control_obstacol.Size = New System.Drawing.Size(70, 25)
+        Me.control_obstacol.TabIndex = 6
+        Me.control_obstacol.TabStop = False
+        '
+        'control_bila
+        '
+        Me.control_bila.Location = New System.Drawing.Point(315, 189)
+        Me.control_bila.Name = "control_bila"
+        Me.control_bila.Size = New System.Drawing.Size(40, 40)
+        Me.control_bila.TabIndex = 1
+        Me.control_bila.TabStop = False
+        '
+        'control_paleta
+        '
+        Me.control_paleta.Location = New System.Drawing.Point(315, 403)
+        Me.control_paleta.Name = "control_paleta"
+        Me.control_paleta.Size = New System.Drawing.Size(200, 25)
+        Me.control_paleta.TabIndex = 0
+        Me.control_paleta.TabStop = False
+        '
+        'obstacol2
+        '
+        Me.obstacol2.Location = New System.Drawing.Point(65, 51)
+        Me.obstacol2.Name = "obstacol2"
+        Me.obstacol2.Size = New System.Drawing.Size(70, 25)
+        Me.obstacol2.TabIndex = 7
+        Me.obstacol2.TabStop = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(13, 9)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 8
+        Me.Button1.Text = "Multiplayer"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(794, 450)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.obstacol2)
+        Me.Controls.Add(Me.control_obstacol)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.control_bila)
+        Me.Controls.Add(Me.control_paleta)
+        Me.Name = "Form1"
+        Me.Text = "Pong"
+        CType(Me.control_obstacol, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.control_bila, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.control_paleta, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.obstacol2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents control_paleta As PictureBox
+    Friend WithEvents control_bila As PictureBox
+    Friend WithEvents miscare_bila As Timer
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents control_obstacol As PictureBox
+    Friend WithEvents obstacol2 As PictureBox
+    Friend WithEvents Button1 As Button
+End Class
---
+++ Laboratorul 9/Form1.vb
@@ -0,0 +1,187 @@
+﻿Public Class Form1
+
+    Dim dx As Integer = 1
+    Dim dy As Integer = 1
+    Dim distanta_bila As Integer = 2
+    Dim distanta_paleta As Integer = 7
+    Dim vieti As Integer = 3
+    Dim puncte As Integer = 0
+
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        control_paleta.Image = My.Resources.paleta
+        control_bila.BackgroundImageLayout = ImageLayout.Stretch
+        control_bila.BackgroundImage = My.Resources.bila
+        control_obstacol.BackgroundImageLayout = ImageLayout.Stretch
+        control_obstacol.BackgroundImage = My.Resources.Obstacol
+        obstacol2.BackgroundImageLayout = ImageLayout.Stretch
+        obstacol2.BackgroundImage = My.Resources.Obstacol
+
+        Label2.Text = vieti
+        Label4.Text = puncte
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles miscare_bila.Tick
+        If control_bila.Left < 0 Then
+            dx = -dx
+
+        End If
+
+        If control_bila.Left + control_bila.Width > Me.Width Then
+            dx = -dx
+
+        End If
+
+        If control_bila.Top < 0 Then
+            dy = -dy
+
+        End If
+
+        If control_bila.Top + control_bila.Height > control_paleta.Top Then
+            Dim centru_bila As Double
+            centru_bila = control_bila.Left + control_bila.Width / 2
+
+            If centru_bila >= control_paleta.Left And centru_bila <= control_paleta.Left + control_paleta.Width Then
+                dy = -dy
+                puncte = puncte + 1
+                Label4.Text = puncte
+
+            Else
+
+                miscare_bila.Enabled = False
+                If vieti > 0 Then
+                    If MessageBox.Show("Ai pierdut. Vrei sa incerci din nou?", "Pong", MessageBoxButtons.OKCancel) = DialogResult.OK Then
+                        control_paleta.Left = Me.Width / 2 - control_paleta.Width / 2
+
+                        control_bila.Top = (Me.Height / 4) * Rnd()
+
+                        miscare_bila.Enabled = True
+                        vieti = vieti - 1
+                        puncte = 0
+                        Label4.Text = puncte
+                        Label2.Text = vieti
+                    End If
+                Else
+                    miscare_bila.Enabled = False
+                    If vieti = 0 Then
+                        If MessageBox.Show("Nu mai ai vieti!", "Pong", MessageBoxButtons.OK) = DialogResult.OK Then
+                            miscare_bila.Enabled = False
+                            Me.Close()
+                        End If
+                    End If
+                End If
+
+
+            End If
+
+
+        End If
+
+
+
+
+        If control_bila.Top + control_bila.Height <= control_obstacol.Top Then
+            Dim centrubw1 As Double = control_bila.Left + control_bila.Width / 2
+            If centrubw1 >= control_obstacol.Left And centrubw1 <= control_obstacol.Left + control_obstacol.Width Then
+                If control_bila.Top + control_bila.Height >= control_obstacol.Top And dy = 1 Then
+                    dy = -dy
+                End If
+            End If
+        End If
+
+
+        If control_bila.Top >= control_obstacol.Top + control_obstacol.Height Then
+            Dim centrubw2 As Double = control_bila.Left + control_bila.Width / 2
+            If centrubw2 >= control_obstacol.Left And centrubw2 <= control_obstacol.Left + control_obstacol.Width Then
+                If control_bila.Top <= control_obstacol.Top + control_obstacol.Height And dy = -1 Then
+                    dy = -dy
+                End If
+            End If
+        End If
+
+        If control_bila.Left + control_bila.Width <= control_obstacol.Left Then
+            Dim centrubh1 As Double = control_bila.Top + control_bila.Height / 2
+            If (centrubh1 >= control_obstacol.Top And centrubh1 <= control_obstacol.Top + control_obstacol.Height) Or (control_bila.Left + control_bila.Width / 2 <= control_obstacol.Top And control_bila.Left + control_bila.Width / 2 >= control_obstacol.Top + control_obstacol.Height) Then
+                If control_bila.Left + control_bila.Width >= control_obstacol.Left And dx = 1 Then
+                    dx = -dx
+                End If
+            End If
+        End If
+
+        If control_bila.Left >= control_obstacol.Left + control_obstacol.Width Then
+            Dim centrubh2 As Double = control_bila.Top + control_bila.Height / 2
+            If centrubh2 >= control_obstacol.Top And centrubh2 <= control_obstacol.Top + control_obstacol.Height Then
+                If control_bila.Left <= control_obstacol.Left + control_obstacol.Width And dx = -1 Then
+                    dx = -dx
+                End If
+            End If
+        End If
+
+
+        If control_bila.Top + control_bila.Height <= obstacol2.Top Then
+            Dim centrubw3 As Double = control_bila.Left + control_bila.Width / 2
+            If centrubw3 >= obstacol2.Left And centrubw3 <= obstacol2.Left + obstacol2.Width Then
+                If control_bila.Top + control_bila.Height >= obstacol2.Top And dy = 1 Then
+                    dy = -dy
+                End If
+            End If
+        End If
+
+        If control_bila.Top >= obstacol2.Top + obstacol2.Height Then
+            Dim centrubw4 As Double = control_bila.Left + control_bila.Width / 2
+            If centrubw4 >= obstacol2.Left And centrubw4 <= obstacol2.Left + obstacol2.Width Then
+                If control_bila.Top <= obstacol2.Top + obstacol2.Height And dy = -1 Then
+                    dy = -dy
+                End If
+            End If
+        End If
+
+        If control_bila.Left + control_bila.Width <= obstacol2.Left Then
+            Dim centrubh3 As Double = control_bila.Top + control_bila.Height / 2
+            If (centrubh3 >= obstacol2.Top And centrubh3 <= obstacol2.Top + obstacol2.Height) Or (control_bila.Left + control_bila.Width / 2 <= obstacol2.Top And control_bila.Left + control_bila.Width / 2 >= obstacol2.Top + obstacol2.Height) Then
+                If control_bila.Left + control_bila.Width >= obstacol2.Left And dx = 1 Then
+                    dx = -dx
+                End If
+            End If
+        End If
+
+
+        If control_bila.Left >= obstacol2.Left + obstacol2.Width Then
+            Dim centrubh4 As Double = control_bila.Top + control_bila.Height / 2
+            If (centrubh4 >= obstacol2.Top And centrubh4 <= obstacol2.Top + obstacol2.Height) Or (control_bila.Left + control_bila.Width / 2 <= obstacol2.Top And control_bila.Left + control_bila.Width / 2 >= obstacol2.Top + obstacol2.Height) Then
+                If control_bila.Left <= obstacol2.Left + obstacol2.Width And dx = -1 Then
+                    dx = -dx
+                End If
+            End If
+        End If
+
+        control_bila.Left = control_bila.Left + distanta_bila * dx
+        control_bila.Top = control_bila.Top + distanta_bila * dy
+
+    End Sub
+
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        If e.KeyCode = 37 Then
+            If control_paleta.Left > 0 Then
+                control_paleta.Left = control_paleta.Left - distanta_paleta
+            End If
+
+        ElseIf e.KeyCode = 39 Then
+            If control_paleta.Left + control_paleta.Width < Me.Width Then
+                control_paleta.Left = control_paleta.Left + distanta_paleta
+
+            End If
+        End If
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Hide()
+        Form2.Show()
+    End Sub
+
+    Private Sub control_paleta_Click(sender As Object, e As EventArgs) Handles control_paleta.Click
+
+    End Sub
+End Class
